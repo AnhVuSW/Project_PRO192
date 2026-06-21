873555a7e0b79c93f5c302cc48b21ad9ec51e080
Trung Nguyênn <tnguyenok1122@gmail.com>
2026-06-21 21:52:41 +0700

    Update MatchRecord.java

--- MatchRecord.java
+++ MatchRecord.java
@@ -8,5 +8,52 @@
  * @author TRUNG NGUYEN
  */
 public class MatchRecord {
+    private String matchId;
+    private String date;
+    private String opponentTeam;
+    private String matchType; 
+
+    
+    public MatchRecord(String matchId, String date, String opponentTeam, String matchType) {
+        this.matchId = matchId;
+        this.date = date;
+        this.opponentTeam = opponentTeam;
+        this.matchType = matchType;
+    }
+
+    
+    public abstract void displayMatchDetails();
+
+    public String getMatchId() { 
+        return matchId; 
+    }
+
+    public void setMatchId(String matchId) { 
+        this.matchId = matchId; 
+    }
+
+    public String getDate() { 
+        return date; 
+    }
+
+    public void setDate(String date) { 
+        this.date = date; 
+    }
+
+    public String getOpponentTeam() { 
+        return opponentTeam; 
+    }
+
+    public void setOpponentTeam(String opponentTeam) { 
+        this.opponentTeam = opponentTeam; 
+    }
+
+    public String getMatchType() { 
+        return matchType; 
+    }
+
+    public void setMatchType(String matchType) { 
+        this.matchType = matchType; 
+    }
     
 }
